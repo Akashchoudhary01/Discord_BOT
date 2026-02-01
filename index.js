65c2609bbf13237744f4a9b0e479b342f3a4e5a7
Akash <akashkrchoudhary007@gmail.com>
2026-02-01 13:50:21 +0530

    initialized\

---
+++ index.js
@@ -0,0 +1,29 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+// Load .env file
+dotenv.config();
+
+// Create client
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// Bot ready
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Listen to messages
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  console.log(message.content);
+});
+
+// Login bot
+client.login(process.env.DISCORD_TOKEN);
